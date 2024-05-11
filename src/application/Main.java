c09a15fad628960f93d6482ac00f7e4f4f74f8b6
Gustavo Daniel <gugadanielalvez@gmail.com>
2024-05-11 11:09:55 -0300

    initialized the Scanner on the Main class

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,10 +1,14 @@
 package application;
 
+import java.util.Scanner;
+
 public class Main {
+
+    private static Scanner sc = new Scanner(System.in);
 
     public static void main(String[] args) {
 
-        
+
 
     }
 
--- src/application/entities/Book.java
+++ src/application/entities/Book.java
@@ -1,4 +1,9 @@
 package application.entities;
 
 public class Book {
+
+    private String bookName, authorName, publisherName;
+    private int releaseYear, pageNumber, isbn;
+    private float value;
+
 }
